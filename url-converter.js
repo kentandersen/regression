a2eda26578e0e81e6b36582928d282ac756e28bf
Hannes Waller <hkwaller@gmail.com>
2016-01-19 13:40:18 +0100

    fix so it doesnt matter if you use old or new screenshot filename

--- url-converter.js
+++ url-converter.js
@@ -1,1 +1,1 @@
-console.log("http://www.gjensidige.no/" + process.argv[2].replace(/\_/gi, "/").replace("-org", ""));
+console.log("http://www.gjensidige.no/" + process.argv[2].replace(/\_/gi, "/").replace("-org", "").replace("-new", ""));
