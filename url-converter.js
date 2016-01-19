221af6ff3332bf632503c8996b5d10913af60dd2
Hannes Waller <hkwaller@gmail.com>
2016-01-19 14:45:33 +0100

    have converter open a browser window with the new url

--- url-converter.js
+++ url-converter.js
@@ -1,1 +1,3 @@
-console.log("http://www.gjensidige.no/" + process.argv[2].replace(/\_/gi, "/").replace("-org", "").replace("-new", ""));
+var open = require('open');
+
+open("http://www.gjensidige.no/" + process.argv[2].replace(/\_/gi, "/").replace("-org", "").replace("-new", ""));
